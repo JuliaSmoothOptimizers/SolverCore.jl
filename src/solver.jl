7f680177fa16759c23c9eadf1b5939c923ad8ff6
tmigot <tangi.migot@gmail.com>
2022-10-02 02:52:04 +0200

    Add `reset!` for `solver` (#47)
    * add `reset!` for solver structure
    
    * add restart tests
    
    Co-authored-by: Dominique <dominique.orban@gmail.com>

--- src/solver.jl
+++ src/solver.jl
@@ -6,6 +6,15 @@ abstract type AbstractSolver end
 abstract type AbstractOptimizationSolver <: AbstractSolver end
 
 """
+    reset!(solver::AbstractOptimizationSolver, model::AbstractNLPModel)
+
+Use in the context of restarting or reusing the `solver` structure.
+Reset the internal fields of `solver` for the `model` before calling `solve!` on the same structure.
+`model` must have the same number of variables, bounds and constraints as that used to instantiate `solver`.
+"""
+function NLPModels.reset!(::AbstractOptimizationSolver, ::AbstractNLPModel) end
+
+"""
     solve!(solver, model; kwargs...)
     solve!(solver, model, stats; kwargs...)
 
---
+++ test/test_restart.jl
@@ -0,0 +1,17 @@
+@testset "test restart" begin
+  nlp = ADNLPModel(x -> dot(x, x) / 2, ones(2), x -> [sum(x .^ 3) - 1], [0.0], [0.0])
+  solver = DummySolver(nlp)
+  stats = GenericExecutionStats(nlp)
+  solve!(solver, nlp, stats, verbose = false)
+  @test stats.status == :first_order
+  # Try with a new intial guess
+  nlp.meta.x0 .= 0.2
+  reset!(solver, nlp)
+  solve!(solver, nlp, stats, verbose = false)
+  @test stats.status == :first_order
+  # Try with a new problem of the same size
+  nlp = ADNLPModel(x -> dot(x, x) / 2, ones(2), x -> [sum(x .^ 3)], [0.0], [0.0])
+  reset!(solver, nlp)
+  solve!(solver, nlp, stats, verbose = false)
+  @test stats.status == :first_order
+end
