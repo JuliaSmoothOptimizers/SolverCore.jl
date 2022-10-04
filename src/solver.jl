ae036d9cbd732c393c38996c19bb5046e66d862a
tmigot <tangi.migot@gmail.com>
2022-10-04 19:20:00 -0400

    Update solver.jl

--- src/solver.jl
+++ src/solver.jl
@@ -7,12 +7,14 @@ abstract type AbstractOptimizationSolver <: AbstractSolver end
 
 """
     reset!(solver::AbstractOptimizationSolver, model::AbstractNLPModel)
+    reset!(solver::AbstractOptimizationSolver)
 
 Use in the context of restarting or reusing the `solver` structure.
 Reset the internal fields of `solver` for the `model` before calling `solve!` on the same structure.
 `model` must have the same number of variables, bounds and constraints as that used to instantiate `solver`.
 """
 function NLPModels.reset!(::AbstractOptimizationSolver, ::AbstractNLPModel) end
+function NLPModels.reset!(::AbstractOptimizationSolver) end
 
 """
     solve!(solver, model; kwargs...)
