b06e6f24bf4536aaf97bb7091c883ce3d22e3d8a
tmigot <tangi.migot@gmail.com>
2025-03-30 11:16:29 -0400

    Drop Julia 1.6 specifics

--- test/test-allocs.jl
+++ test/test-allocs.jl
@@ -1,13 +1,11 @@
-if VERSION ≥ v"1.6"
-  @testset "Test allocations of solver specific" begin
-    nlp = BROWNDEN()
-    stats = GenericExecutionStats(nlp) # stats = GenericExecutionStats(nlp, solver_specific = Dict{Symbol, Bool}())
-    function fake_solver(stats)
-      set_solver_specific!(stats, :test, true)
-      return stats
-    end
-    @allocated fake_solver(stats)
-    a = @allocated fake_solver(stats)
-    @test a == 0
+@testset "Test allocations of solver specific" begin
+  nlp = BROWNDEN()
+  stats = GenericExecutionStats(nlp) # stats = GenericExecutionStats(nlp, solver_specific = Dict{Symbol, Bool}())
+  function fake_solver(stats)
+    set_solver_specific!(stats, :test, true)
+    return stats
   end
+  @allocated fake_solver(stats)
+  a = @allocated fake_solver(stats)
+  @test a == 0
 end
